6d716e9ca4e247ad17f07f718898f1656ad92e4e
eriknastesjo <erna21@student.bth.se>
2022-11-28 13:12:09 +0000

    Copy react_native folder from dockerized-react-native

--- react_native/App.tsx
+++ react_native/App.tsx
@@ -1,16 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View, TouchableOpacity, Button } from 'react-native';
 import { useState } from 'react';
 import { Base, Typography } from './styles/index';
-import testModel from './models/testModel'
+import { IP } from '@env'
+
 
 export default function App() {
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState("Data not fetched yet");
 
 
-  function fetchData() {
-    {/* Todo: testa att hämta via testModel här när vi vet route */ }
+  async function fetchData() {
+    try {
+      const response = await fetch(`http://${IP}:3000/test`);
+      const result = await response.json();
+      console.log(result);
+      setData(result.testing);
+
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
 
   return (
@@ -18,9 +27,11 @@ export default function App() {
       <Button
         color='#4F4C4A'
         onPress={fetchData}
-        title="Fetch some goddamn data"
-      />
-      { data }
+        title="Fetch dataTest"
+        />
+      <Text>IP is {IP}</Text>
+      <Text>Data:</Text>
+      <Text>{data}</Text>
     </View>
   );
 }
